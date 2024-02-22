314b62fee25084a5d0223997a3fffd7d5760c6a1
Mihir Patel <mihirom9876@gmail.com>
2024-02-22 14:22:52 +0530

    Minor Changes

--- src/main.c
+++ src/main.c
@@ -1,5 +1,3 @@
-#include <assert.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,50 +7,50 @@
 #include "utils/logging.h"
 
 int file_size(FILE *file) {
-    fseek(file, 0, SEEK_END);
-    int size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    return size;
+  fseek(file, 0, SEEK_END);
+  int size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  return size;
 }
 
 char *file_contents(char *filepath) {
-    FILE *file = fopen(filepath, "r");
-    if (!file) {
-        char *message = (char *)malloc(400 * sizeof(char));
-        sprintf(message, "File not Found at path : %s", filepath);
-        log_system_errors(message);
-        free(message);
-        exit(0);
+  FILE *file = fopen(filepath, "r");
+  if (!file) {
+    char *message = (char *)malloc(400 * sizeof(char));
+    sprintf(message, "File not Found at path : %s", filepath);
+    log_system_errors(message);
+    free(message);
+    exit(0);
+  } else {
+    int size = file_size(file);
+    char *contents = (char *)malloc((size + 1) * sizeof(char));
+    size_t bytes_read = fread(contents, 1, size, file);
+    if (bytes_read != (size_t)size) {
+      free(contents);
+      return NULL;
     } else {
-        int size = file_size(file);
-        char *contents = (char *)malloc((size + 1) * sizeof(char));
-        size_t bytes_read = fread(contents, 1, size, file);
-        if (bytes_read != (size_t)size) {
-            free(contents);
-            return NULL;
-        } else {
-            contents[size] = '\0';
-            return contents;
-        }
+      contents[size] = '\0';
+      return contents;
     }
+  }
 }
 
 void print_usage(char **argv) {
-    printf("%sUSAGE: %s <path_to_file_to_compile>%s\n", RED, argv[0],
-           COLOR_RESET);
+  printf("%sUSAGE: %s <path_to_file_to_compile>%s\n", RED, argv[0],
+         COLOR_RESET);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        print_usage(argv);
-        exit(0);
-    } else {
-        char *filename = argv[1];
-        char *contents = file_contents(filename);
-        // printf("%s", contents);
-        Node ROOT;
-        Error err = parse(contents, ROOT);
-        log_error(&err);
-    }
-    return 0;
+  if (argc < 2) {
+    print_usage(argv);
+    exit(0);
+  } else {
+    char *filename = argv[1];
+    char *contents = file_contents(filename);
+    // printf("%s", contents);
+    Node ROOT;
+    Error err = parse(contents, ROOT);
+    log_error(&err);
+  }
+  return 0;
 }
